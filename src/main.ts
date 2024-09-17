4787eb659c4ffdb108e836c66ffd435c197d61dc
Druyeah <you@example.com>
2024-09-16 23:33:30 -0400

    actualizacion de origin cors

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,12 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '../../', 'public'));
   console.log(join(__dirname, '../../', 'public'));
 
-  // Configura CORS
+  // Configura CORS para permitir tanto el dominio del frontend como otros dominios relevantes
   app.enableCors({
-    origin: 'https://localhost:4200',
+    origin: ['https://diegocaceres.online', 'https://api.diegocaceres.online'], // Permitir ambos dominios
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
 
   await app.listen(3000);
