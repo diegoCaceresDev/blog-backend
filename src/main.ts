71b21fa1e183626e35c26de3d21c368d9be4f533
Diego <you@example.com>
2024-10-07 16:52:48 -0300

    modificaciones en el servicio de imagenes

--- src/main.ts
+++ src/main.ts
@@ -8,23 +8,13 @@ async function bootstrap() {
 
   // Configura CORS dinámico basado en el origen de la solicitud
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = ['http://localhost:4200', 'https://blog.diegocaceres.online'];
-      
-      // Si el origen está en la lista de permitidos, lo acepta
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: ['http://localhost:4200', 'https://blog.diegocaceres.online'], // Agrega ambos dominios
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
-  // Configuración para servir archivos estáticos desde la carpeta 'public'
-  app.useStaticAssets(join(__dirname, '../../', 'public'));
+  app.useStaticAssets(join(__dirname, '../../', 'public/images'));
   console.log(join(__dirname, '../../', 'public'));
 
   await app.listen(3000);
